9f3078a9f7fd6df8be184b6e6795710ef5071847
theepan <neotechmonk@gmail.com>
2018-10-08 14:35:09 +1100

    final

--- .compiled/imdb.js
+++ .compiled/imdb.js
@@ -12,9 +12,13 @@ var imdb = require("imdb-api");
 //API Access Key from omdbapi.com
 var omdbAPIKey = "5bea3702";
 
-function retrieveMovie(movieName) {
+function retrieveMovie(movieName, apiKey) {
+  //treat apiKey as an optional paramter
+  if (typeof apiKey === "undefined") {
+    apiKey = omdbAPIKey;
+  }
   return new Promise(function (resolve, reject) {
-    var client = new imdb.Client({ apiKey: omdbAPIKey });
+    var client = new imdb.Client({ apiKey: apiKey });
     var movie = void 0;
     client.get({ name: movieName }).then(function (_movie) {
       resolve(_movie);
--- src/imdb.js
+++ src/imdb.js
@@ -7,9 +7,13 @@ var imdb = require("imdb-api");
 //API Access Key from omdbapi.com
 const omdbAPIKey = "5bea3702";
 
-function retrieveMovie(movieName) {
+function retrieveMovie(movieName, apiKey) {
+  //treat apiKey as an optional paramter
+  if (typeof apiKey === "undefined") {
+    apiKey = omdbAPIKey;
+  }
   return new Promise((resolve, reject) => {
-    const client = new imdb.Client({ apiKey: omdbAPIKey });
+    const client = new imdb.Client({ apiKey: apiKey });
     let movie;
     client
       .get({ name: movieName })
@@ -21,6 +25,5 @@ function retrieveMovie(movieName) {
       });
   });
 }
-
 
 export { retrieveMovie };
